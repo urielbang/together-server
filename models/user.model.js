674190d6954e280003ad296aa1b0fdaa17d8f18b
urielbang <uri32655@gmail.com>
2024-06-15 23:29:01 +0300

    user role

--- models/user.model.js
+++ models/user.model.js
@@ -4,6 +4,11 @@ const UserShema = moongose.Schema({
   name: { type: "String", required: true },
   email: { type: "String", required: true },
   password: { type: "String", required: true },
+  role: {
+    type: String,
+    enum: ["user", "guide", "lead-guide", "admin"],
+    default: "user",
+  },
   imageUrl: { type: String },
   publicId: { type: String },
 });
